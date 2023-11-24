9484831e004b7174cbdbb91827ac1d92b4a5a308
HenrickyL <HenrickyL1@gmail.com>
2023-11-24 09:15:11 -0300

    base

---
+++ sci-fi card/src/components/SciFiCard/index.tsx
@@ -0,0 +1,15 @@
+import {SciFiCardSty} from './style.ts'
+type SciFiCardProp = {
+
+}
+
+const SciFiCard : React.FC<SciFiCardProp> = (prop:SciFiCardProp)=>{
+    return (
+        <SciFiCardSty>
+            
+        </SciFiCardSty>
+    )
+}
+
+
+export {SciFiCard}
---
+++ sci-fi card/src/components/SciFiCard/style.ts
@@ -0,0 +1,7 @@
+import {styled} from 'styled-components'
+
+const SciFiCardSty = styled.div`
+`
+
+
+export {SciFiCardSty}
